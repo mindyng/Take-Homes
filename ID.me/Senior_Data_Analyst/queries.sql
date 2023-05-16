c43612c53d8e5e8710f922af0efd2c3882c139b6
Mindy Ng <mindyng85@gmail.com>
2023-05-16 13:10:26 -0700

    Update queries.sql

--- ID.me/Senior_Data_Analyst/queries.sql
+++ ID.me/Senior_Data_Analyst/queries.sql
@@ -1,9 +1,44 @@
 -- Ref: (creation of tables and queries) https://www.db-fiddle.com/f/cGXjLQJ3Gz1vy7ucVMjngx/2
+-- 3. The customer service team has asked one of the data analysts to develop a dashboard to illustrate the monthly interaction volume of the accounts with the 10 highest number of active developer licenses. They are looking for various breakdowns by account, month, interaction channel, category, service representative, and interaction status.
 
+-- You have been tasked with designing a model to provide the data for the analyst. How would you structure the output? Include the query you would use to create the model.
+
+--filter on active licenses
+--by account (and month), get count of licenses
+--rank in descending order highest amount of licenses per account
+--make sure final table has month, interaction channel, category, service representative, interaction status by joining customer_licenses with customer_interactions (after aggregation in customer_licenses on active licenses per month from interactions, account)
+
+WITH unnesting AS (
 SELECT account_id
 , license_data ->> 'license_id' AS license_id
 , license_data ->> 'role' AS role
 , license_data ->> 'status' AS status
 , created_at
 , updated_at
-FROM customer_licenses;
+FROM customer_licenses
+)
+
+, active_dev_flag AS (SELECT account_id
+, SUM(CASE WHEN role = 'developer' AND status = 'active' THEN 1 ELSE 0 END) AS total_active_dev_lic
+FROM unnesting
+GROUP BY 1
+)
+
+, activity_rank AS (SELECT account_id
+, total_active_dev_lic
+, DENSE_RANK() OVER (ORDER BY total_active_dev_lic DESC) AS active_account_rank
+FROM active_dev_flag
+)
+
+SELECT EXTRACT(MONTH FROM created_at) AS interaction_month
+, most_active_dev_lic.account_id
+, channel AS interaction_channel
+, category
+, service_rep
+, status
+FROM (SELECT account_id
+FROM activity_rank 
+WHERE active_account_rank <= 10) AS most_active_dev_lic 
+JOIN customer_interactions
+ON most_active_dev_lic.account_id = customer_interactions.account_id
+ORDER BY 1;
