7ee67ef758bbacf012fc8d65b215603bf9b83823
Mindy Ng <mindyng85@gmail.com>
2023-06-28 12:12:41 -0700

    Update order_aggregation_new_features.sql

--- Tovala/queries/order_aggregation_new_features.sql
+++ Tovala/queries/order_aggregation_new_features.sql
@@ -1,21 +1,44 @@
+with cts_no_outliers as (
+select * 
+from customer_term_summary 
+where term_start_date not in ('2020-11-23', '2020-12-21', '2021-11-22', '2021-12-20', '2022-11-21', '2022-12-19')
+)
+
+, last_active as (
+select customer_id
+, MAX(term_start_date) as last_active
+from cts_no_outliers
+where subscription_status = 'active'
+group by 1
+)
+
+, recency as (select distinct t1.customer_id
+, MAX(term_start_date) OVER () - last_active AS weeks_since_last_tx
+from cts_no_outliers as t1
+left join last_active as t2
+on t1.customer_id = t2.customer_id
+)
+
+
 --what do i want to capture in general about orders per customer
-, cts_agg as (select cts.customer_id
+, cts_agg as (
+select cts.customer_id
 , weeks_since_last_tx
-, AVG(case when year(term_start_date) IN (2020,2021) then 1 else 0 end) as pandemic_flag
-, AVG(case when month(term_start_date) IN (1,11,12) then 1 else 0 end) as seasonal_break_flag
+, MODE(case when year(term_start_date) IN (2020,2021) then 1 else 0 end) as pandemic_flag
+, MODE(case when month(term_start_date) IN (1,11,12) then 1 else 0 end) as seasonal_break_flag
 , MAX(cohort_week_without_holidays) as tenure
 , MODE(case when order_statu IS NULL THEN 'paused or canceled sub' ELSE order_statu end) AS most_frequent_order_status
 , MODE(subscription_status) as most_freq_sub
 , MAX(running_total_fulfilled_order_count) as most_fulfilled_orders
-, MAX(case when last_eight_week_order_count is null then 0 else last_eight_week_order_count end) as eight_wk_window_most_placed_orders
+, AVG(case when last_eight_week_order_count is null then 0 else last_eight_week_order_count end) as eight_wk_window_most_placed_orders
 , AVG(consecutive_skip_count) as avg_consec_skips
-, AVG(CASE WHEN order_statu = 'skipped' THEN 1 ELSE 0 END) AS skipped
+, MAX(running_total_fulfilled_order_count) / MAX(cohort_week_without_holidays) as orders_per_tenure
+, MODE(CASE WHEN order_statu = 'skipped' THEN 1 ELSE 0 END) AS skipped
 
-from customer_term_summary as cts
+from cts_no_outliers as cts
 join recency as r
 ON cts.customer_id = r.customer_id
---where term_start_date NOT IN ('2020-11-23', '2020-12-21', '2021-11-22', '2021-12-20', '2022-11-21', '2022-12-19')
-group by 1,2
+group by 1, 2
 )
 
 select cts_agg.customer_id
@@ -28,6 +51,7 @@ select cts_agg.customer_id
 , most_fulfilled_orders
 , eight_wk_window_most_placed_orders
 , avg_consec_skips
+, orders_per_tenure
 , skipped
 
 , FIRST_ORDER_SIZE
@@ -78,3 +102,4 @@ left join customer_facts as cf
 on cts_agg.customer_id = cf.customer_id
 left join oven_orders as oo
 on cts_agg.customer_id = oo.customer_id
+order by 1
